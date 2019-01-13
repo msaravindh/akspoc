efe4367c74f649817bed743d0ed611b5118653e8
msaravindh <arsrini@microsoft.com>
2019-01-13 19:56:26 +0530

    Update SetupMySQLDB.sh

--- YAML/SetupMySQLDB.sh
+++ YAML/SetupMySQLDB.sh
@@ -1,2 +1,4 @@
+(sleep 30
 mysql -u root -pMicrosoft@1 -e "CREATE USER 'webpocdbuser' IDENTIFIED BY 'Microsoft@1'; create database employee_db; GRANT ALL PRIVILEGES ON employee_db.* TO 'webpocdbuser'"
 mysql -u root -pMicrosoft@1 employee_db < /tmp/employee_db.sql
+exit)&
