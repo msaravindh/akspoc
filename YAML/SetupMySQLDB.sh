d9fd57fa30cabcc0d915841a0bd2d1742a1d2862
msaravindh <arsrini@microsoft.com>
2019-01-25 13:33:16 +0530

    Update SetupMySQLDB.sh
    Pick up MySQL Password from Environment Variable

--- YAML/SetupMySQLDB.sh
+++ YAML/SetupMySQLDB.sh
@@ -1,4 +1,4 @@
 (sleep 30
-mysql -u root -pMicrosoft@1 -e "CREATE USER 'webpocdbuser' IDENTIFIED BY 'Microsoft@1'; create database employee_db; GRANT ALL PRIVILEGES ON employee_db.* TO 'webpocdbuser'"
-mysql -u root -pMicrosoft@1 employee_db < /tmp/employee_db.sql
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER 'webpocdbuser' IDENTIFIED BY 'Microsoft@1'; create database employee_db; GRANT ALL PRIVILEGES ON employee_db.* TO 'webpocdbuser'"
+mysql -u root -p"MYSQL_ROOT_PASSWORD" employee_db < /tmp/employee_db.sql
 exit)&
